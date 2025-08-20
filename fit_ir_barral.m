8270a7a8655d61423749fdcbf800a3688b23ff58
Mark Bydder <17042435+marcsous@users.noreply.github.com>
2025-08-20 07:42:45 -0700

    Add files via upload

--- fit_ir_barral.m
+++ fit_ir_barral.m
@@ -10,12 +10,10 @@ function [T1 x phi ci95] = fit_ir_barral(TI,data,TR,T1,lambda)
 %  data is an array of complex data points (NxM)
 %  TR (optional) is the repetition time (scalar)
 %  T1 (optional) is an initial T1 estimate (scalar)
-%  lambda (optional) is an "ideality" constraint (scalar)
+%  lambda (optional) is an "ideality" penalty on x(1)+x(2)/2=0 (scalar)
 %
 % Model: [x(1) + x(2)*exp(-TI/T1)] * exp(i*phi)
 % Ref. Barral (2010) doi.org/10.1002/mrm.22497
-%
-% lambda penalizes [x(1) + x(2)/2] (should be 0)
 %
 %% check inputs
 if isreal(data) && all(data>=0)
@@ -50,10 +48,10 @@ nrhs = size(data,2);
 
 % optimization
 opts = optimset('display','off','TolFun',1e-10);
-[R1,~,~,~,~,H] = fminunc(@(R)myfunc(R,TI,TR,data,lambda),R1,opts);
+[R1,~,~,~,~,H] = fminunc(@(R)myfun(R,TI,TR,data,lambda),R1,opts);
 
 % 95% confidence interval
-[resnorm f x phi] = myfunc(R1,TI,TR,data,lambda);
+[resnorm f x phi] = myfun(R1,TI,TR,data,lambda);
 if isreal(data)
     v = numel(data)-1-2*nrhs; % [T1 A1 A2]
 else
@@ -95,7 +93,7 @@ text(0.03,0.83,str,'Units','Normalized','FontName','FixedWidth')
 fprintf('Fitted T1: %.3f ± %.3f\n',T1,ci95);
 
 %% function: [x(1) + x(2)*exp(-R*t)] * exp(i*phi)
-function [resnorm f x phi] = myfunc(R,ti,tr,b,lambda)
+function [resnorm f x phi] = myfun(R,ti,tr,b,lambda)
 
 % multiple coils (nc)
 [nti nc] = size(b);
@@ -107,15 +105,14 @@ A = [ones(nti,1) exp(-R*ti)];
 L = [1 1/2]*lambda;
 
 % doi.org/10.1016/j.laa.2010.07.011
+M = A'*A+L'*L; Ab = A'*b;
 if 1
     % linear
-    M = A'*A+L'*L;
-    x = pinv(M)*(A'*b);
+    x = pinv(M)*Ab;
     phi = 0; % unused
 else
     % phase constrained
-    M = real(A'*A+L'*L);
-    invM = pinv(M); Ab = A'*b;
+    invM = pinv(real(M)); 
     phi = angle(dot(conj(Ab),invM*Ab))/2;
     x = invM*real(Ab.*exp(-i*phi));
 end
@@ -123,7 +120,6 @@ end
 % function and residual norm
 f = A*(x.*exp(i*phi));
 resnorm = norm(f-b,'fro')^2;
-f = f(1:nti,:);
 
 % cosmetic change of sign
 if nargout>2
