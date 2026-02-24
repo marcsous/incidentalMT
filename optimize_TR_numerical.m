2601c21c384aa0acf6ca4c40e999f63ec86c7ad3
Mark Bydder <17042435+marcsous@users.noreply.github.com>
2026-02-24 15:06:41 -0800

    Update optimize_TR_numerical.m

--- optimize_TR_numerical.m
+++ optimize_TR_numerical.m
@@ -1,4 +1,4 @@
-% code to optimize TR for a given T1 to null and T1 of interest
+% code to optimize TR for a given T1-to-null and T1-of-interest
 clear
 
 T1null = 4000; % T1 to null
@@ -6,18 +6,18 @@ T1int = 790;   % T1 of interest
 TElast = 100;  % time of last echo (fast spin echo)
 
 % avoid singularity at zero
-initial_guess = 3.57*T1int;
+initial_est = 3.57*T1int;
 
 % find the minimum of -efficiency
 myfun = @(TR)-efficiency(TR,T1null,T1int,TElast);
-TRopt = fminsearch(myfun,initial_guess,optimset('display','off'));
+TRopt = fminsearch(myfun,initial_est,optimset('display','off'));
 
 % display results
-[E TI M C] = efficiency(TRopt,T1null,T1int,TElast);
-fprintf('TRopt=%f TI=%f M=%+f E=%f C=%f\n',TRopt,TI,M,E,C);
+[E TI M] = efficiency(TRopt,T1null,T1int,TElast);
+fprintf('TRopt=%f TI=%f M=%+f E=%f\n',TRopt,TI,M,E);
 
-%% function to calculate efficiency/contrast
-function [E TI M C] = efficiency(TR,T1null,T1int,TElast)
+%% function to calculate efficiency
+function [E TI M] = efficiency(TR,T1null,T1int,TElast)
 
 % TI needed for T1null
 TI = T1null * (log(2) - log(1+exp(-(TR-TElast)/T1null)));
@@ -25,12 +25,7 @@ TI = T1null * (log(2) - log(1+exp(-(TR-TElast)/T1null)));
 % magnetization at T1int
 M = 1 - 2*exp(-TI/T1int) + exp(-(TR-TElast)/T1int);
 
-% efficiency normalized to spin echo
+% efficiency at T1int
 E = abs(M) / TR^(1/2);
-E = E / (0.64 / T1int^(1/2));
-
-% contrast at T1int normalized to spin echo
-C = abs(exp(-(TR-TElast)/T1int)*(TR-TElast) - 2*exp(-TI/T1int)*TI) / T1int^2;
-C = C / (2*exp(-2) / T1int);
 
 end
